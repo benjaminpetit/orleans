3b8d7d884c66445d64cfb8eb8af713c94cea3795
Benjamin Petit <bpetit@microsoft.com>
2020-01-23 14:00:47 -0800

    Basic IPluggableGrainDirectory

---
+++ src/Orleans.Core.Abstractions/GrainDirectory/IPluggableGrainDirectory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.GrainDirectory
+{
+    public interface IPluggableGrainDirectory
+    {
+        Task<GrainAddress> Register(GrainAddress address);
+
+        Task Unregister(GrainAddress address);
+
+        Task<List<GrainAddress>> Lookup(string grainId);
+    }
+
+    public struct GrainAddress
+    {
+        public string SiloAddress { get; set; }
+        public string GrainId { get; set; }
+        public string ActivationId { get; set; }
+    }
+}
