c86997f6643fceb4c0628737def68e1d7cb77ab9
Reuben Bond <reuben.bond@gmail.com>
2024-07-25 15:02:38 -0700

    Azure Core Event listener which will log way too much

--- test/Extensions/TesterAzureUtils/AzureStorageOperationOptionsExtensions.cs
+++ test/Extensions/TesterAzureUtils/AzureStorageOperationOptionsExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using Azure.Core;
 using Azure.Data.Tables;
 using Azure.Identity;
@@ -8,6 +9,7 @@ namespace Tester.AzureUtils
 {
     public static class AzureStorageOperationOptionsExtensions
     {
+        private static AzureCoreEventListener _listener = new();
         public static DefaultAzureCredentialOptions Options = new DefaultAzureCredentialOptions
         {
             AdditionallyAllowedTenants = { "*" },
@@ -15,7 +17,8 @@ namespace Tester.AzureUtils
             {
                 LoggedHeaderNames = { "x-ms-request-id" },
                 LoggedQueryParameters = { "api-version" },
-                IsAccountIdentifierLoggingEnabled = true
+                IsAccountIdentifierLoggingEnabled = true,
+                IsLoggingEnabled = true,
             }
         };
 
@@ -36,10 +39,33 @@ namespace Tester.AzureUtils
                 Console.WriteLine($"'{key}'='{env[key]}'");
             }
 
+            Console.WriteLine($"TenantId: {Options.TenantId}");
+            Console.WriteLine($"ManagedIdentityClientId: {Options.ManagedIdentityClientId}");
+            Console.WriteLine($"WorkloadIdentityClientId: {Options.WorkloadIdentityClientId}");
+
+            _ = _listener.ToString();
+
             return TestDefaultConfiguration.UseAadAuthentication
                 ? new(TestDefaultConfiguration.TableEndpoint, Credential)
                 : new(TestDefaultConfiguration.DataConnectionString);
         }
+
+        internal sealed class AzureCoreEventListener : EventListener
+        {
+            protected override void OnEventSourceCreated(EventSource source)
+            {
+                if (source.Name.Equals("Azure-Core"))
+                {
+                    EnableEvents(source, EventLevel.Verbose, EventKeywords.All, new Dictionary<string, string>());
+                }
+            }
+
+            protected override void OnEventWritten(EventWrittenEventArgs eventData)
+            {
+                Console.WriteLine($"[{eventData.EventName}] {eventData.Message} {string.Join(", ", eventData.PayloadNames.Zip(eventData.Payload).Select((k, v) => $"{k}={v}"))}.");
+            }
+        }
+            
 
         public static Orleans.GrainDirectory.AzureStorage.AzureStorageOperationOptions ConfigureTestDefaults(this Orleans.GrainDirectory.AzureStorage.AzureStorageOperationOptions options)
         {
